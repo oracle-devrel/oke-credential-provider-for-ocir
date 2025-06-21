0975082afc245f17dfad32c2d065fc0035bac445
gotsysdba <john.lathouwers@oracle.com>
2025-06-21 20:39:35 +0100

    Closes #4

--- cmd/provider.go
+++ cmd/provider.go
@@ -7,15 +7,35 @@ package main
 
 import (
 	"flag"
+	"os"
+	"time"
 
+	"github.com/gofrs/flock"
 	"github.com/devrocks/credential-provider-oke/internal/helpers"
 	"github.com/devrocks/credential-provider-oke/internal/provider"
 )
 
 func main() {
 	var configType string
-	flag.StringVar(&configType, "config", "", "Type the absolute path to yaml file with provider configuration. If it's ommited, program reads from environment variables (REGISTRY_TOKEN_PATH, DEFAULT_USER, REGISTRY_PROTOCOL, OCIR_AUTH_METHOD).")
+	flag.StringVar(&configType, "config", "", "Path to config YAML or environment config")
 	flag.Parse()
+
+	lockFilePath := "/var/lib/kubelet/credential-provider.lock"
+	fileLock := flock.New(lockFilePath)
+
+	locked, err := fileLock.TryLock()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if !locked {
+		// Lock is held by another process, sleep then exit
+		// that process will then be able to use the cache and reduce requests
+		time.Sleep(1 * time.Second)
+		os.Exit(0)
+	}
+	defer fileLock.Unlock()
+
 	config := helpers.ReadConfig(configType)
 	provider.GetCredentialProviderResponse(config)
 }
--- go.mod
+++ go.mod
@@ -5,13 +5,14 @@ go 1.23.0
 toolchain go1.24.4
 
 require (
+	github.com/gofrs/flock v0.12.1
 	github.com/oracle/oci-go-sdk v24.3.0+incompatible
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	golang.org/x/sys v0.22.0 // indirect
 )
